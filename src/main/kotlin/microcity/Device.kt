0fcf42d4881a575860cc8a82c46fae558cd722b9
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-06-23 12:53:26 +0200

    chore: fix syntax errors

--- src/main/kotlin/microcity/Device.kt
+++ src/main/kotlin/microcity/Device.kt
@@ -7,12 +7,23 @@ import org.protelis.vm.ExecutionEnvironment
 import microcity.Utils.role
 
 object Device {
+
+    private val DEVICE_PREFIX: String = "PtDevice"
+    private val EMPTY_PREFIX: String = ""
+
     @JvmStatic
-    fun getID(ctx: AlchemistExecutionContext<*>): DeviceUID = ctx.getDeviceUID()
+    fun getId(ctx: AlchemistExecutionContext<*>): Int =
+            ctx.getDeviceUID().toString().replace(DEVICE_PREFIX, EMPTY_PREFIX).toInt()
+
+    @JvmStatic
+    fun getCoordinates(ctx: AlchemistExecutionContext<*>): Tuple = ctx.getCoordinates()
 
     @JvmStatic
     fun has(ctx: AlchemistExecutionContext<*>, molecule: String): Boolean = ctx.getExecutionEnvironment().has(molecule)
 
     @JvmStatic
     fun get(ctx: AlchemistExecutionContext<*>, molecule: String): Any = ctx.getExecutionEnvironment().get(molecule)
+
+    @JvmStatic
+    fun <T> put(ctx: AlchemistExecutionContext<*>, molecule: String, value: T): Any = ctx.getExecutionEnvironment().put(molecule, value)
 }
--- src/main/kotlin/microcity/Utils.kt
+++ src/main/kotlin/microcity/Utils.kt
@@ -4,10 +4,20 @@ import it.unibo.alchemist.protelis.AlchemistExecutionContext
 import org.protelis.lang.datatype.Tuple
 import microcity.Device.has
 import microcity.Device.get
+import microcity.Device.put
 
-public object Utils {
+object Utils {
 
-    @JvmStatic
     fun role(ctx: AlchemistExecutionContext<*>, role: String): Boolean =
         has(ctx, role) && (get(ctx, role) as Boolean)
+
+    object Guests {
+
+        fun satisfy(ctx: AlchemistExecutionContext<*>, value: Boolean) {
+            put(ctx, "satisfied", value)
+        }
+
+        fun isSatisfied(ctx: AlchemistExecutionContext<*>): Boolean =
+                has(ctx, "satisfied") && (get(ctx, "satisfied") as Boolean)
+    }
 }
