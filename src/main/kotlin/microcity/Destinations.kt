6b726105b2e8803bc1a1c96e7def5aa11d5de6e4
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-06-23 15:23:25 +0200

    chore: redirect guests to a destination

--- src/main/kotlin/microcity/Destinations.kt
+++ src/main/kotlin/microcity/Destinations.kt
@@ -1,30 +1,27 @@
 package microcity
 
 import it.unibo.alchemist.protelis.AlchemistExecutionContext
-import org.protelis.lang.datatype.Tuple
-import microcity.Positions.Position
-import microcity.Positions.getPositions
-import microcity.Device.get
-import microcity.Device.getId
 import microcity.Device.getCoordinates
-import microcity.Device.has
+import microcity.Positions.getPositions
 import microcity.Utils.Guests.isSatisfied
+import microcity.Utils.Guests.satisfy
+import org.protelis.lang.datatype.Tuple
 import kotlin.random.Random
 
 object Destinations {
 
     @JvmStatic
-    fun getNext(ctx: AlchemistExecutionContext<*>): Tuple = when {
-        isSatisfied(ctx) -> {
-            val positions = getPositions(ctx)
-            val next = Random.nextInt(0, positions.size)
-            satisfy(false)
-            return positions.find { it.id == next }?.coordinates ?: getCoordinates()
-        }
-        else -> {
-
+    fun getNext(ctx: AlchemistExecutionContext<*>, current: Tuple): Tuple {
+        val positions = getPositions(ctx)
+        return when {
+            (isSatisfied(ctx) && positions.isNotEmpty()) -> {
+                val next = Random.nextInt(0, positions.size)
+                val position = positions.find { it.id == next }?.coordinates ?: getCoordinates(ctx)
+                satisfy(ctx, position != getCoordinates(ctx))
+                position
+            }
+            else -> current
         }
     }
-
 
 }
