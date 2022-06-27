2c6b33d909113c3c6bc8bf7a3537d358088358d3
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-06-27 13:39:26 +0200

    chore: Guests are satisfied and then leave the attraction

--- src/main/kotlin/microcity/Queues.kt
+++ src/main/kotlin/microcity/Queues.kt
@@ -4,13 +4,17 @@ import it.unibo.alchemist.protelis.AlchemistExecutionContext
 import microcity.Device.getCoordinates
 import microcity.Device.getId
 import microcity.Positions.Position
+import microcity.Utils.Activities.getGuestsPerRound
+import microcity.Utils.Activities.getQueue
+import microcity.Utils.Activities.updateQueue
 import microcity.Utils.Molecules.ACTIVITY
 import microcity.Utils.role
 import microcity.Utils.Guests.enqueue
+import microcity.Utils.Guests.isSatisfied
+import microcity.Utils.Molecules.GUEST
 import org.protelis.lang.datatype.Tuple
 
 object Queues {
-
     @JvmStatic
     fun enqueue(ctx: AlchemistExecutionContext<*>, destination: Tuple) {
         enqueue(ctx, destination == getCoordinates(ctx))
@@ -23,4 +27,20 @@ object Queues {
             .filter { it.id != getId(ctx) }
         else -> arrayListOf(Position(getId(ctx), getCoordinates(ctx)))
     }
+
+    @JvmStatic
+    fun satisfyGuests(ctx: AlchemistExecutionContext<*>): List<Position> = when {
+        role(ctx, ACTIVITY) -> ArrayList(getQueue(ctx).take(getGuestsPerRound(ctx)))
+        else -> arrayListOf()
+    }
+
+    @JvmStatic
+    fun satisfactionUnion(ctx: AlchemistExecutionContext<*>, a: List<Position>, b: List<Position>): List<Position> = ArrayList(when {
+        role(ctx, GUEST) -> a.union(b).filter { it.id == getId(ctx) }
+        else -> a.union(b).toList()
+    })
+
+
+
+
 }
