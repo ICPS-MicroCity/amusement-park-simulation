8a4e4661dc0d0c70c98a42f2b59d262a92177a2f
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-06-27 13:39:26 +0200

    chore: Guests are satisfied and then leave the attraction

--- src/main/kotlin/microcity/Utils.kt
+++ src/main/kotlin/microcity/Utils.kt
@@ -5,9 +5,12 @@ import microcity.Device.get
 import microcity.Device.getCoordinates
 import microcity.Device.has
 import microcity.Device.put
+import microcity.Positions.Position
 import microcity.Utils.Molecules.DESTINATION
 import microcity.Utils.Molecules.ENQUEUED
 import microcity.Utils.Molecules.GUEST
+import microcity.Utils.Molecules.QUEUE
+import microcity.Utils.Molecules.ROUND_CAPACITY
 import microcity.Utils.Molecules.SATISFIED
 import org.protelis.lang.datatype.Tuple
 
@@ -16,6 +19,11 @@ object Utils {
     fun role(ctx: AlchemistExecutionContext<*>, role: String): Boolean =
         has(ctx, role) && (get(ctx, role) as Boolean)
 
+    @JvmStatic
+    fun debug(toDebug: Any) {
+        println(toDebug)
+    }
+
     object Molecules {
         const val POSITIONS: String = "org:protelis:microcity:positions"
         const val ACTIVITY: String = "activity"
@@ -23,6 +31,8 @@ object Utils {
         const val SATISFIED: String = "satisfied"
         const val ENQUEUED: String = "enqueued"
         const val DESTINATION: String = "org:protelis:microcity:destination"
+        const val QUEUE: String = "org:protelis:microcity:queue"
+        const val ROUND_CAPACITY: String = "roundCapacity"
     }
 
     object Guests {
@@ -34,6 +44,7 @@ object Utils {
         fun getDestination(ctx: AlchemistExecutionContext<*>): Tuple =
             if (has(ctx, DESTINATION)) get(ctx, DESTINATION) as Tuple else getCoordinates(ctx)
 
+        @JvmStatic
         fun satisfy(ctx: AlchemistExecutionContext<*>, value: Boolean) {
             put(ctx, SATISFIED, value)
         }
@@ -48,6 +59,24 @@ object Utils {
 
         @JvmStatic
         fun isSatisfied(ctx: AlchemistExecutionContext<*>): Boolean =
-                has(ctx, SATISFIED) && (get(ctx, SATISFIED) as Boolean)
+            has(ctx, SATISFIED) && (get(ctx, SATISFIED) as Boolean)
+
+    }
+
+    object Activities {
+        @JvmStatic
+        fun getQueue(ctx: AlchemistExecutionContext<*>): List<Position> = when {
+            has(ctx, QUEUE) -> get(ctx, QUEUE) as List<Position>
+            else -> arrayListOf()
+        }
+
+        @JvmStatic
+        fun updateQueue(ctx: AlchemistExecutionContext<*>, queue: List<Position>) {
+            put(ctx, QUEUE, queue)
+        }
+
+        @JvmStatic
+        fun getGuestsPerRound(ctx: AlchemistExecutionContext<*>): Int =
+            (get(ctx, ROUND_CAPACITY) as Double).toInt()
     }
 }
