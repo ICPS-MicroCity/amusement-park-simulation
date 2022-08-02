7a3fa8431955a194b33b87cd8a55260b1a398eb8
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-08-02 11:17:25 +0200

    chore: update policies

--- src/main/kotlin/microcity/policy/ShortestQueuePolicy.kt
+++ src/main/kotlin/microcity/policy/ShortestQueuePolicy.kt
@@ -4,8 +4,12 @@ import it.unibo.alchemist.protelis.AlchemistExecutionContext
 import microcity.Utils.Visitors.getDestination
 import microcity.Utils.Visitors.getQueues
 import org.protelis.lang.datatype.Tuple
+import kotlin.random.Random
 
 class ShortestQueuePolicy : NextPolicy {
     override fun getNext(ctx: AlchemistExecutionContext<*>): Tuple =
-        getQueues(ctx).filter { getDestination(ctx) != it.attraction.coordinates }.sortedBy { it.visitors.size }[0].attraction.coordinates
+        getQueues(ctx).filter { getDestination(ctx) != it.attraction.coordinates }
+            .filter { it.visitors.size == getQueues(ctx).minBy { q -> q.visitors.size }.visitors.size }
+            .let { it[Random.nextInt(0, it.size)] }
+            .attraction.coordinates
 }
