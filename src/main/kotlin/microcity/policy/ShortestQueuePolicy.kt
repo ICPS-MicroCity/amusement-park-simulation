daa668cd5a419d200668bab753e8a6ab23c10535
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-09-01 17:38:53 +0200

    chore: add attraction class encapsulating position

--- src/main/kotlin/microcity/policy/ShortestQueuePolicy.kt
+++ src/main/kotlin/microcity/policy/ShortestQueuePolicy.kt
@@ -8,8 +8,8 @@ import kotlin.random.Random
 
 class ShortestQueuePolicy : NextPolicy {
     override fun getNext(ctx: AlchemistExecutionContext<*>): Tuple =
-        getQueues(ctx).filter { getDestination(ctx) != it.attraction.coordinates }
+        getQueues(ctx).filter { getDestination(ctx) != it.attraction.position.coordinates }
             .filter { it.visitors.size == getQueues(ctx).minBy { q -> q.visitors.size }.visitors.size }
             .let { it[Random.nextInt(0, it.size)] }
-            .attraction.coordinates
+            .attraction.position.coordinates
 }
