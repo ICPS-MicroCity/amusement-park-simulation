9ade0ae24344b28a019e5fdb46ed9b5890bec993
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-09-01 16:51:19 +0200

    chore: save previous destination in a proper molecule

--- src/main/kotlin/microcity/Destinations.kt
+++ src/main/kotlin/microcity/Destinations.kt
@@ -3,20 +3,23 @@ package microcity
 import it.unibo.alchemist.protelis.AlchemistExecutionContext
 import microcity.Device.getCoordinates
 import microcity.Positions.getPositions
+import microcity.Utils.Visitors.getDestination
 import microcity.Utils.Visitors.getNextPolicy
 import microcity.Utils.Visitors.getQueues
 import microcity.Utils.Visitors.isSatisfied
 import microcity.Utils.Visitors.satisfy
+import microcity.Utils.Visitors.setPreviousDestination
 import org.protelis.lang.datatype.Tuple
 
 object Destinations {
 
-    fun getDestination(ctx: AlchemistExecutionContext<*>): Tuple =
-        Utils.Visitors.getDestination(ctx)
-
     @JvmStatic
     fun getNext(ctx: AlchemistExecutionContext<*>, current: Tuple): Tuple = when {
-        isSatisfied(ctx) && getPositions(ctx).size > 10 && getQueues(ctx).size > 1 -> getNextPolicy(ctx).getNext(ctx)
+        isSatisfied(ctx) && getPositions(ctx).size > 10 && getQueues(ctx).size > 1 ->
+            getNextPolicy(ctx).getNext(ctx).also {
+                if (it != getDestination(ctx))
+                    setPreviousDestination(ctx, getDestination(ctx))
+            }
         else -> current
     }
 
