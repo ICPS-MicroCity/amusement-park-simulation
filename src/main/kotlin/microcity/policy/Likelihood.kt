84e0f52e51282659d8ebe4b4024ee9245bb7cd13
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-09-02 16:41:38 +0200

    chore: reduce likelihood of previous destination

--- src/main/kotlin/microcity/policy/Likelihood.kt
+++ src/main/kotlin/microcity/policy/Likelihood.kt
@@ -1,5 +1,6 @@
 package microcity.policy
 
+import org.protelis.lang.datatype.Tuple
 import kotlin.math.exp
 
 fun likelihood(popularity: Double, laziness: Double): Double = popularity / laziness
@@ -14,10 +15,15 @@ fun normalize(x: Double, min: Double, max: Double): Double = (x - min) / (max - 
 fun queueTime(queueSize: Int, capacity: Int, duration: Double): Double = (queueSize / capacity) * duration
 
 fun queueTimeScore(queueTime: Double): Int = when {
-     queueTime < 300 -> 1 // 5 minutes
-     queueTime < 1200 -> 2 // 20 minutes
-     queueTime < 1800 -> 3 // 30 minutes
-     queueTime < 2700 -> 4 // 45 minutes
-     queueTime < 3600 -> 5 // 60 minutes
-     else -> 6
+    queueTime < 300 -> 1 // 5 minutes
+    queueTime < 1200 -> 2 // 20 minutes
+    queueTime < 1800 -> 3 // 30 minutes
+    queueTime < 2700 -> 4 // 45 minutes
+    queueTime < 3600 -> 5 // 60 minutes
+    else -> 6
+}
+
+fun Double.considerPreviousDestination(position: Tuple, previousDestination: Tuple): Double = when (position) {
+    previousDestination -> this * 0.5
+    else -> this
 }
