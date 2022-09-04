dd2ce783632b41737b0d662b73e7e4e56edac89d
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-09-04 13:09:49 +0200

    chore: remove strategy variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
                 // Namely, force the simulator not to use graphical output.
                 args("-hl", "-t", maxTime)
                 args("-b")
-                args("-var", "strategy", "scenarioSeed", "simulationSeed")
+                //args("-var", "strategy", "scenarioSeed", "simulationSeed")
             } else {
                 // A graphics environment should be available, so load the effects for the UI from the "effects" folder
                 // Effects are expected to be named after the simulation file
