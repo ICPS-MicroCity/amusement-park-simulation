1854c7b452c828bff4ed18b757ae173073df1a65
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-09-28 16:39:25 +0200

    chore: add molecule defined time distribution

--- src/main/kotlin/it/unibo/alchemist/model/implementations/timedistributions/MoleculeDefinedDiracComb.kt
+++ src/main/kotlin/it/unibo/alchemist/model/implementations/timedistributions/MoleculeDefinedDiracComb.kt
@@ -1,4 +1,7 @@
 package it.unibo.alchemist.model.implementations.timedistributions
 
-class MoleculeDefinedDiracComb {
-}
+import it.unibo.alchemist.model.interfaces.Molecule
+import it.unibo.alchemist.model.interfaces.Node
+
+class MoleculeDefinedDiracComb<T>(node: Node<T>, molecule: Molecule)
+    : DiracComb<T>(node.getConcentration(molecule) as Double)
